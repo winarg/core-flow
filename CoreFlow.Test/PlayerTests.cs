5357ac72e6c744746f03cc89873b42fa2b4d1c70
Anargyros Roussos <roussos_a@DataCommunicationSA583.onmicrosoft.com>
2021-05-05 16:45:05 +0300

    test: added test with selection

--- CoreFlow.Test/PlayerTests.cs
+++ CoreFlow.Test/PlayerTests.cs
@@ -6,7 +6,7 @@
     using CoreFlow.Model.Enums;
 
     [TestFixture]
-    public class PlayerTests
+    public class PlayerTests : TestBase
     {
         private CoreFlowEngine _engine;
 
@@ -48,6 +48,44 @@
             }
 
             Assert.AreEqual(CoreFlowStatus.Completed, testFlow.Status);
+        }
+
+        [Test]
+        public void Engine_ForkFlow()
+        {
+            CoreFlowEntity testFlow = new();
+
+            // Tasks
+            var taskStart = new CoreFlowTask("Start", CoreFlowTaskType.Start);
+            var taskAction = new CoreFlowTask("Action", CoreFlowTaskType.Action);
+            var taskEnd1 = new CoreFlowTask("End1", CoreFlowTaskType.End);
+            var taskEnd2 = new CoreFlowTask("End2", CoreFlowTaskType.End);
+
+            testFlow.Tasks.Add(taskStart);
+            testFlow.Tasks.Add(taskAction);
+            testFlow.Tasks.Add(taskEnd1);
+
+            // Transitions
+            var transitionStartToAction = new CoreFlowTransition("Start", taskStart, taskAction);
+            var transitionActionToEnd1 = new CoreFlowTransition("Complete", taskAction, taskEnd1);
+            var transitionActionToEnd2 = new CoreFlowTransition("Fail", taskAction, taskEnd2);
+
+            testFlow.Transitions.Add(transitionStartToAction);
+            testFlow.Transitions.Add(transitionActionToEnd1);
+            testFlow.Transitions.Add(transitionActionToEnd2);
+
+            FixIds(testFlow);
+
+            // Actions
+            Assert.IsTrue(_engine.StartFlow(testFlow));
+
+            Assert.AreEqual(CoreFlowTaskStatus.Active, taskAction.Status);
+
+            Assert.IsTrue(_engine.CompleteTask(testFlow, taskAction, transitionActionToEnd1.Id));
+
+            Assert.AreEqual(CoreFlowStatus.Completed, testFlow.Status);
+            Assert.AreEqual(CoreFlowTaskStatus.Completed, taskEnd1.Status);
+            Assert.AreEqual(CoreFlowTaskStatus.NotStarted, taskEnd2.Status);
         }
     }
 }
---
+++ CoreFlow.Test/TestBase.cs
@@ -0,0 +1,20 @@
+﻿namespace CoreFlow.Test
+{
+    using CoreFlow.Model.Entities;
+
+    public abstract class TestBase
+    {
+        public void FixIds(CoreFlowEntity flow)
+        {
+            for (int i = 0; i < flow.Tasks.Count; i++)
+            {
+                flow.Tasks[i].Id = i;
+            }
+
+            for (int i = 0; i < flow.Transitions.Count; i++)
+            {
+                flow.Transitions[i].Id = i;
+            }
+        }
+    }
+}
